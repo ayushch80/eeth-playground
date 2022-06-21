06211778bca919d62d15284702226f2c0b087b5c
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-21 23:24:32 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -2,6 +2,6 @@ import { etherToGwei } from './ether-to-gwei.js';
 import { etherToWei } from './ether-to-wei.js';
 import { gweiToEther } from './gwei-to-ether.js';
 import { weiToEther } from './wei-to-ether.js';
-import { toUtf8Bytes } from './to-utf8-bytes.js';
-const term = toUtf8Bytes('ethereum');
+import { splitSignature } from './split-signature.js';
+const term = '0x60bc4ed91f2021aefe7045f3f77bd12f87eb733aee24bd1965343b3c27b3971647252185b7d2abb411b01b5d1ac4ab41ea486df1e9b396758c1aec6c1b6eee331b';
 console.log(term);
