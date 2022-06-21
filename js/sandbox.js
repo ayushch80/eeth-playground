e66165383a7caa5e057b9523ce8a482e9ae85835
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-21 23:59:53 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -3,3 +3,9 @@ import { etherToWei } from './ether-to-wei.js';
 import { gweiToEther } from './gwei-to-ether.js';
 import { weiToEther } from './wei-to-ether.js';
 import { splitSignature } from './split-signature.js';
+const myCode = document.getElementById("mKey");
+const usrInpt = myMne.elements.namedItem("userInput").value;
+document.getElementById("btn_click").addEventListener("click", getData);
+function getData() {
+  usrInpt
+}
