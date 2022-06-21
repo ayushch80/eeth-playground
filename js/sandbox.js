0c3217b758c375b28fe73d66b683205387a3f582
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-21 23:04:32 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,4 +1,4 @@
 import { etherToGwei } from './ether-to-gwei.js';
 import { etherToWei } from './ether-to-wei.js';
-const term = etherToGwei('1000').toString();
+const term = etherToWei('1000').toString();
 console.log(term);
