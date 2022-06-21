8493edab2e861f07357cab225e4d35ae42c394e5
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-21 23:14:29 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -2,5 +2,6 @@ import { etherToGwei } from './ether-to-gwei.js';
 import { etherToWei } from './ether-to-wei.js';
 import { gweiToEther } from './gwei-to-ether.js';
 import { weiToEther } from './wei-to-ether.js';
-const term = gweiToEther('1000').toString();
+import { toUtf8Bytes } from './to-utf8-bytes.js';
+const term = toUtf8Bytes('ethereum');
 console.log(term);
