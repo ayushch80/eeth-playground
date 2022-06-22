354b087940ffff88faedda826998df598cc1508d
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 21:08:43 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -7,6 +7,6 @@ import { keccak256 } from './keccak256.js'
 import { toChecksumAddress } from './to-checksum-address.js'
 import { computePublicKey } from './compute-public-key.js'
 import { computeAddress } from './compute-address.js';
-import { arrayify , concat , hexConcat , hexDataLength , hexDataSlice } from './bytes.js';
-const term = hexDataSlice([20, 6, 48], 0, 2);
+import { arrayify , concat , hexConcat , hexDataLength , hexDataSlice , hexStripZeros } from './bytes.js';
+const term = hexStripZeros([0, 0, 0, 48]);
 console.log(term); 
