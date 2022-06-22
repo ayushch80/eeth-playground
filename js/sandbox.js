2ba110b784d4d55d3af47d06cd53ce347a7c52af
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 12:32:05 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -11,7 +11,7 @@ const usrInpt = getEl('userInput')
 document.body.onkeyup = function() {
     iFrame.open()
     iFrame.writeln(
-    '<!doctype html><html><body><script src="js/ether-to-gwei.js"/>' +
+    '<!doctype html><html><body><script src="js/ether-to-gwei.js" type="module"/>' +
     '<script>' +
     usrInpt.value +
     '</script></body></html>'
