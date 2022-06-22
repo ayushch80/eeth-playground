0a5a5401ca805c19338fa3d5a2d0df8bac8f5ff1
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 21:15:51 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -8,5 +8,6 @@ import { toChecksumAddress } from './to-checksum-address.js'
 import { computePublicKey } from './compute-public-key.js'
 import { computeAddress } from './compute-address.js';
 import { arrayify , concat , hexConcat , hexDataLength , hexDataSlice , hexStripZeros , hexValue , hexZeroPad , hexlify } from './bytes.js';
-const term = hexlify(14);
+import { isAddress } from './is-address.js';
+const term = isAddress('0xc0deaf6bd3f0c6574a6a625ef2f22f62a5150eab');
 console.log(term); 
