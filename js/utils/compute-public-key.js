31aa7798670161438f152a6fc7f61f7567c36092
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 23:21:44 +0530

    Update compute-public-key.js

--- js/utils/compute-public-key.js
+++ js/utils/compute-public-key.js
@@ -1,4 +1,4 @@
-import { Point } from './secp256k1.js';
+import { Point } from '../libraries/secp256k1.js';
 import { hexlify } from './bytes.js';
 export function computePublicKey(privKey) {
     privKey = hexlify(privKey).slice(2);
