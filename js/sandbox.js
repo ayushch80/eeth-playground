9947d50cc338a9ca06628dd683bea27a37e0aecc
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 21:22:45 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -7,7 +7,7 @@ import { keccak256 } from './keccak256.js'
 import { toChecksumAddress } from './to-checksum-address.js'
 import { computePublicKey } from './compute-public-key.js'
 import { computeAddress } from './compute-address.js';
-import { arrayify , concat , hexConcat , hexDataLength , hexDataSlice , hexStripZeros , hexValue , hexZeroPad , hexlify , isBytes } from './bytes.js';
+import { arrayify , concat , hexConcat , hexDataLength , hexDataSlice , hexStripZeros , hexValue , hexZeroPad , hexlify , isBytes , isBytesLike , isHexString , stripZeros , zeroPad } from './bytes.js';
 import { isAddress } from './is-address.js';
 const term = isBytes([1, 2, 3]);
 console.log(term); 
