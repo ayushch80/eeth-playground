880467b0e6251aac9e039b1005da7b34772db090
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 21:46:00 +0530

    Create clean-transaction-receipt.js

---
+++ js/providers/clean-transaction-receipt.js
@@ -0,0 +1,35 @@
+import { tinyBig } from '../tiny-big.js';
+import { toChecksumAddress } from '../to-checksum-address.js';
+import { cleanLog } from './clean-log.js';
+import { cleanTransaction } from './clean-transaction.js';
+import { hexToDecimal } from './hex-to-decimal.js';
+export function cleanTransactionReceipt(transactionReceipt) {
+    const cleanedTransaction = cleanTransaction(transactionReceipt);
+    const cleanedTransactionReceipt = Object.assign({}, cleanedTransaction);
+    Object.keys(transactionReceipt).forEach((key) => {
+        if (!transactionReceipt[key])
+            return;
+        switch (key) {
+            case 'status':
+                cleanedTransactionReceipt[key] = Number(hexToDecimal(transactionReceipt[key]));
+                break;
+            case 'contractAddress':
+                if (transactionReceipt[key]) {
+                    cleanedTransactionReceipt[key] = toChecksumAddress(transactionReceipt[key]);
+                }
+                break;
+            case 'cumulativeGasUsed':
+            case 'effectiveGasPrice':
+            case 'gasUsed':
+                cleanedTransactionReceipt[key] = tinyBig(hexToDecimal(transactionReceipt[key]));
+                break;
+            case 'logs':
+                transactionReceipt[key].forEach((log, index) => {
+                    cleanedTransactionReceipt[key][index] = cleanLog(log, true);
+                });
+        }
+    });
+    cleanedTransactionReceipt.byzantium =
+        cleanedTransactionReceipt.blockNumber >= 4370000;
+    return cleanedTransactionReceipt;
+}
