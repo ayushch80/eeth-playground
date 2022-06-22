5282f9902622126192da3b99c99e65c64398a821
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 23:22:58 +0530

    Update gwei-to-ether.js

--- js/utils/gwei-to-ether.js
+++ js/utils/gwei-to-ether.js
@@ -1,5 +1,5 @@
-import { tinyBig } from './tiny-big.js';
-import { validateType } from './validate-type.js';
+import { tinyBig } from '../shared/tiny-big/tiny-big.js';
+import { validateType } from '../shared/validate-type.js';
 export function gweiToEther(gweiQuantity) {
     validateType(gweiQuantity, ['string', 'number', 'object']);
     const result = tinyBig(gweiQuantity).div('1000000000');
