22afde86f7953c4c203e97e73059d8f84fa1b5c3
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 15:44:15 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -3,4 +3,6 @@ import { etherToWei } from './ether-to-wei.js';
 import { gweiToEther } from './gwei-to-ether.js';
 import { weiToEther } from './wei-to-ether.js';
 import { splitSignature } from './split-signature.js';
-console.log(etherToGwei(1000).toString());
+import { keccak256 } from './keccak256.js'
+const term = keccak256('hi')
+console.log(term); 
