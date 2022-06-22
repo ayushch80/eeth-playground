d01f10a507344495610cfa828445b79e612d617a
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 18:42:40 +0530

    Update keccak256.js

--- js/keccak256.js
+++ js/keccak256.js
@@ -1,7 +1,7 @@
 import { keccak } from './keccak.js';
 export function keccak256(data) {
     if (typeof data === 'string') {
-        Data = (data.replace(/^0x/, ''), 'hex');
+      let Data = (data.replace(/^0x/, ''), 'hex');
     }
     else {
         console.log("Input cant be processed");
