df1ea3d380c9bac58ea82a486286facf7c7ba662
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 23:55:36 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,3 +1,3 @@
 import { etherToWei, etherToGwei, isAddress, tinyBig, toChecksumAddress, weiToEther, gweiToEther, splitSignature, computeAddress, computePublicKey, TinyBig, isBytes, isBytesLike, arrayify, concat, stripZeros, zeroPad, isHexString, hexlify, hexDataLength, hexDataSlice, hexConcat, hexValue, hexStripZeros, hexZeroPad, keccak256 } from './index.js';
-const term = isBytes([1, 2, 3]);
+const term = computeAddress('0x2f2c419acf4a1da8c1ebea75bb3fcfbd3ec2aa3bf0162901ccdc2f38b8f92427',);
 console.log(term); 
