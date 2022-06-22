ed281bdcbf2ba1e59a97a364965110ae51296ab8
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 23:44:09 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,3 +1,3 @@
-import { etherToWei, etherToGwei, isAddress, tinyBig, toChecksumAddress, weiToEther, gweiToEther, splitSignature, computeAddress, computePublicKey, TinyBig, isBytes, isBytesLike, arrayify, concat, stripZeros, zeroPad, isHexString, hexlify, hexDataLength, hexDataSlice, hexConcat, hexValue, hexStripZeros, hexZeroPad, keccak256 } from './index';
+import { etherToWei, etherToGwei, isAddress, tinyBig, toChecksumAddress, weiToEther, gweiToEther, splitSignature, computeAddress, computePublicKey, TinyBig, isBytes, isBytesLike, arrayify, concat, stripZeros, zeroPad, isHexString, hexlify, hexDataLength, hexDataSlice, hexConcat, hexValue, hexStripZeros, hexZeroPad, keccak256 } from './index.js';
 const term = isBytes([1, 2, 3]);
 console.log(term); 
