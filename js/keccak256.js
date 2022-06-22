dd30c94d67b18b58e7601cd2ae9842816efadc3b
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 16:44:43 +0530

    Update keccak256.js

--- js/keccak256.js
+++ js/keccak256.js
@@ -1,11 +1,11 @@
-import { Keccak } from './sha3.js';
+import { keccak } from './keccak.js';
 export function keccak256(data) {
     let bufferableData;
     if (typeof data === 'string') {
-        bufferableData = Buffer.from(data.replace(/^0x/, ''), 'hex');
+        bufferableData = (data.replace(/^0x/, ''), 'hex');
     }
     else {
-        bufferableData = Buffer.from(data);
+        bufferableData = (data);
     }
     const keccak = new Keccak(256);
     const addressHash = '0x' + keccak.update(bufferableData).digest('hex');
