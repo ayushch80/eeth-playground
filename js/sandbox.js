1078839f6493714568525f78694daff4163be19f
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 19:46:11 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -5,5 +5,6 @@ import { weiToEther } from './wei-to-ether.js';
 import { splitSignature } from './split-signature.js';
 import { keccak256 } from './keccak256.js'
 import { toChecksumAddress } from './to-checksum-address.js'
-const term = toChecksumAddress('0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359')
+import { computePublicKey } from './compute-public-key.js'
+const term = computePublicKey('0xb27cc8dea0177d910110e8d3ec5480d56c723abf433529f4063f261ffdb9297c')
 console.log(term); 
