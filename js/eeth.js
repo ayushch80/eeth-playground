79173c3a61cb0c605baa9140e0cd3893c1811a3c
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 12:47:16 +0530

    Update eeth.js

--- js/eeth.js
+++ js/eeth.js
@@ -1085,51 +1085,6 @@ export function scientificStrToDecimalStr(scientificString) {
         return `${isNegative ? '-' : ''}${toReturn}`;
     }
 }
-export function scientificStrToDecimalStr(scientificString) {
-    if (!scientificString.match(/e/i)) {
-        return stripTrailingZeroes(scientificString);
-    }
-    let [base, power] = scientificString.split(/e/i);
-    const isNegative = Number(base) < 0;
-    base = base.replace('-', '');
-    base = stripTrailingZeroes(base);
-    const [wholeNumber, fraction = ''] = base.split('.');
-    if (Number(power) === 0) {
-        return `${isNegative ? '-' : ''}${stripTrailingZeroes(base)}`;
-    }
-    else {
-        const includesDecimal = base.includes('.');
-        if (!includesDecimal) {
-            base = `${base}.`;
-        }
-        base = base.replace('.', '');
-        const baseLength = base.length;
-        let splitPaddedNumber;
-        if (Number(power) < 0) {
-            if (wholeNumber.length < Math.abs(Number(power))) {
-                base = base.padStart(baseLength + Math.abs(Number(power)) - wholeNumber.length, '0');
-            }
-            splitPaddedNumber = base.split('');
-            if (wholeNumber.length < Math.abs(Number(power))) {
-                splitPaddedNumber = ['.', ...splitPaddedNumber];
-            }
-            else {
-                splitPaddedNumber.splice(splitPaddedNumber.length - Math.abs(Number(power)), 0, '.');
-            }
-        }
-        else {
-            if (fraction.length < Math.abs(Number(power))) {
-                base = base.padEnd(baseLength + Math.abs(Number(power)) - fraction.length, '0');
-            }
-            splitPaddedNumber = base.split('');
-            if (fraction.length > Math.abs(Number(power))) {
-                splitPaddedNumber.splice(splitPaddedNumber.length - Math.abs(Number(power)), 0, '.');
-            }
-        }
-        const toReturn = stripTrailingZeroes(splitPaddedNumber.join(''));
-        return `${isNegative ? '-' : ''}${toReturn}`;
-    }
-}
 export const validateType = (value, allowedTypes) => {
     if (!allowedTypes.includes(typeof value)) {
         throw new Error(`${allowedTypes.join(' or ')} required. Received ${typeof value}`);
