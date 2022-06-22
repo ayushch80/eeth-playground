aabe8d9d1481e930571203ffd1760c76a7fc5ced
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 23:30:47 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,13 +1,3 @@
-import { etherToGwei } from './ether-to-gwei.js';
-import { etherToWei } from './ether-to-wei.js';
-import { gweiToEther } from './gwei-to-ether.js';
-import { weiToEther } from './wei-to-ether.js';
-import { splitSignature } from './split-signature.js';
-import { keccak256 } from './keccak256.js'
-import { toChecksumAddress } from './to-checksum-address.js'
-import { computePublicKey } from './compute-public-key.js'
-import { computeAddress } from './compute-address.js';
-import { arrayify , concat , hexConcat , hexDataLength , hexDataSlice , hexStripZeros , hexValue , hexZeroPad , hexlify , isBytes , isBytesLike , isHexString , stripZeros , zeroPad } from './bytes.js';
-import { isAddress } from './is-address.js';
+import * from './index';
 const term = isBytes([1, 2, 3]);
 console.log(term); 
