6f1c156d7e4e71233d73375ae9853642557977ba
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 15:33:24 +0530

    Create pi-shuffles.js

---
+++ js/pi-shuffles.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PI_SHUFFLES = [10, 7, 11, 17, 18, 3, 5, 16, 8, 21, 24, 4, 15, 23, 19, 13, 12, 2, 20, 14, 22, 9, 6, 1];
+var _default = PI_SHUFFLES;
+export default void 0;
