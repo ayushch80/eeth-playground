c3f4da121b1b9796c61b4a182b669f7c7b10723b
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-22 20:13:09 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -6,5 +6,6 @@ import { splitSignature } from './split-signature.js';
 import { keccak256 } from './keccak256.js'
 import { toChecksumAddress } from './to-checksum-address.js'
 import { computePublicKey } from './compute-public-key.js'
-const term = computePublicKey('0xb27cc8dea0177d910110e8d3ec5480d56c723abf433529f4063f261ffdb9297c')
+import { computeAddress } from './compute-address.js';
+const term = computeAddress('0xb27cc8dea0177d910110e8d3ec5480d56c723abf433529f4063f261ffdb9297c')
 console.log(term); 
