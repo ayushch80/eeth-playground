80824323958c5abb8ab567b5d3b188724c653f56
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-23 12:26:37 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,4 +10,5 @@ import { toChecksumAddress } from './utils/to-checksum-address.js';
 import { weiToEther } from './utils/wei-to-ether.js';
 import { isBytes, isBytesLike, arrayify, concat, stripZeros, zeroPad, isHexString, hexlify, hexDataLength, hexDataSlice, hexConcat, hexValue, hexStripZeros, hexZeroPad,} from './utils/bytes.js';
 import { keccak256 } from './utils/keccak256.js';
-export { etherToWei, etherToGwei, isAddress, tinyBig, toChecksumAddress, weiToEther, gweiToEther, splitSignature, computeAddress, computePublicKey, TinyBig, isBytes, isBytesLike, arrayify, concat, stripZeros, zeroPad, isHexString, hexlify, hexDataLength, hexDataSlice, hexConcat, hexValue, hexStripZeros, hexZeroPad, keccak256};
+import { toUtf8Bytes } from './utils/to-utf8-bytes.js';
+export { etherToWei, etherToGwei, isAddress, tinyBig, toChecksumAddress, weiToEther, gweiToEther, splitSignature, computeAddress, computePublicKey, TinyBig, isBytes, isBytesLike, arrayify, concat, stripZeros, zeroPad, isHexString, hexlify, hexDataLength, hexDataSlice, hexConcat, hexValue, hexStripZeros, hexZeroPad, keccak256, toUtf8Bytes };
