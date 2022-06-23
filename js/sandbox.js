72eb2b119c7a623f3830fd0fd9659b9365c3e317
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-23 11:41:35 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,9 +1,9 @@
 import { etherToWei, etherToGwei, isAddress, tinyBig, toChecksumAddress, weiToEther, gweiToEther, splitSignature, computeAddress, computePublicKey, TinyBig, isBytes, isBytesLike, arrayify, concat, stripZeros, zeroPad, isHexString, hexlify, hexDataLength, hexDataSlice, hexConcat, hexValue, hexStripZeros, hexZeroPad, keccak256 } from './index.js';
-document.getElementById("btn_click").addEventListener("click", getData);
-function getData() {
+//document.getElementById("btn_click").addEventListener("click", getData);
+//function getData() {
   const myCode = document.getElementById("mKey");
   const myData = myCode.elements.namedItem("userInput").value;
   var jsCode = `console.clear();
                 ${myData}`
   $(".output").html(jsCode)
-}
+//}
