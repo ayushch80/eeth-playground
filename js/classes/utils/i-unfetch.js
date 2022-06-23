2f5b0d4971993ff1ffee175d57bb0a6b4e162eab
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-23 14:47:33 +0530

    Update i-unfetch.js

--- js/classes/utils/i-unfetch.js
+++ js/classes/utils/i-unfetch.js
@@ -1,2 +1,1 @@
-import * as require from 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js'
 module.exports = self.fetch || (self.fetch = require('unfetch.js').default || require('unfetch.js'));
