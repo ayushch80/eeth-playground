2a52aa60880b01faa063e12a7d43637c379eaadb
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-23 13:34:01 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -3,7 +3,7 @@ function getData() {
   const myCode = document.getElementById("mKey");
   const myData = myCode.elements.namedItem("userInput").value;
   var jsCode = `<script type="module">
-                import { etherToWei, etherToGwei, isAddress, tinyBig, toChecksumAddress, weiToEther, gweiToEther, splitSignature, computeAddress, computePublicKey, TinyBig, isBytes, isBytesLike, arrayify, concat, stripZeros, zeroPad, isHexString, hexlify, hexDataLength, hexDataSlice, hexConcat, hexValue, hexStripZeros, hexZeroPad, keccak256, toUtf8Bytes, hashMessage } from './js/index.js';
+                import { etherToWei, etherToGwei, isAddress, tinyBig, toChecksumAddress, weiToEther, gweiToEther, splitSignature, computeAddress, computePublicKey, TinyBig, isBytes, isBytesLike, arrayify, concat, stripZeros, zeroPad, isHexString, hexlify, hexDataLength, hexDataSlice, hexConcat, hexValue, hexStripZeros, hexZeroPad, keccak256, toUtf8Bytes, hashMessage, pack, solidityKeccak256 } from './js/index.js';
                 console.clear();
                 ${myData}</script>`
   $(".output").html(jsCode)
