c8afd9237bd8d6335b7c52afa51c6ba45185f99b
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-25 19:05:17 +0530

    Update keccak256.js

--- js/utils/keccak256.js
+++ js/utils/keccak256.js
@@ -1,5 +1,5 @@
-import sha3 from '../libraries/js-sha3.js';
-import { BufferShim } from '../libraries/buffer.js'
+import { keccak } from '../libraries/keccak.js';
+import { BufferShim } from '../libraries/buffer.js';
 import { arrayify } from "./bytes.js";
 export function keccak256(data) {
     let bufferableData;
@@ -9,6 +9,7 @@ export function keccak256(data) {
     else {
         bufferableData = BufferShim.from(data);
     }
-    const addressHash = '0x' + sha3.keccak_256(arrayify(data));
+    let Keccak = keccak(256)
+    const addressHash = '0x' + Keccak.update(bufferableData).hex;
     return addressHash;
 }
