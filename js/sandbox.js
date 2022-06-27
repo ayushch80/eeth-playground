8f4d35123b98a93eea1a999f4ae9a46193cccdcf
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2022-06-27 10:18:21 +0530

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -5,6 +5,6 @@ function getData() {
   var jsCode = `<script type="module">
                 import { etherToWei, etherToGwei, isAddress, tinyBig, toChecksumAddress, weiToEther, gweiToEther, splitSignature, computeAddress, computePublicKey, TinyBig, isBytes, isBytesLike, arrayify, concat, stripZeros, zeroPad, isHexString, hexlify, hexDataLength, hexDataSlice, hexConcat, hexValue, hexStripZeros, hexZeroPad, keccak256, toUtf8Bytes, hashMessage, pack, solidityKeccak256, FallthroughProvider, JsonRpcProvider, jsonRpcProvider } from './js/index.js';
                 console.clear();
-                &lt;${myData}&gt;</script>`
+                ${myData}</script>`
   $(".output").html(jsCode)
 }
